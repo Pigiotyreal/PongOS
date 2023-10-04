9420c7fb4546b23db4a2214cf6fef91810ee37b3
Pigioty <pigiotyreal@gmail.com>
2023-10-04 12:41:36 -0400

    16 bit printing

--- boot/main.asm
+++ boot/main.asm
@@ -1,5 +1,11 @@
 [org 0x7c00]
 
+[bits 16]
+
+mov ebx, msgreal
+call print_16
+xor ebx, ebx
+
 mov bp, 0x8000
 mov sp, bp
 
@@ -16,15 +22,31 @@ jmp $
 %include "boot/switch.asm"
 %include "boot/disk.asm"
 
+print_16:
+    mov ah, 0x0e
+
+print_16_loop:
+    mov al, [ebx]
+    cmp al, 0
+    je print_16_end
+    int 0x10
+    inc ebx
+    jmp print_16_loop
+
+print_16_end:
+    ret
+
 jmp $
 
 [bits 32]
 BEGIN_PM:
     mov ebx, msgprotected
     call print_string
+    xor ebx, ebx
     jmp $
 
-msgprotected db "Hello protected mode!", 0
+msgreal db "Booting PongOS..", 0
+msgprotected db "Loaded into protected mode!", 0
 
 times 510-($-$$) db 0
 dw 0xaa55
