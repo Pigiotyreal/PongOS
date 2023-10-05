d5a246d069141ba2b1f113abc753edf7f066e2a8
Pigioty <pigiotyreal@gmail.com>
2023-10-05 14:43:38 -0400

    printchar

--- kernel/main.c
+++ kernel/main.c
@@ -1,18 +1,17 @@
 void main() {
-    char* vga = (char*)0xb8000;
-    *vga = 'H';
-    vga += 2;
-    *vga = 'e';
-    vga += 2;
-    *vga = 'l';
-    vga += 2;
-    *vga = 'l';
-    vga += 2;
-    *vga = 'o';
-    vga += 2;
-    *vga = '!';
+    printChar('H', 0, 1);
+    printChar('e', 0, 2);
+    printChar('l', 0, 3);
+    printChar('l', 0, 4);
+    printChar('o', 0, 5);
 
     while (1) {
         asm("hlt");
     }
+}
+
+void printChar(char c, int row, int col) {
+    char* vga = (char*)0xb8000;
+    vga += 2 * (row * 80 + col);
+    *vga = c;
 }
