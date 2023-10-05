6a789f082f4366dc6fed69cd302efcf12be50601
Pigioty <pigiotyreal@gmail.com>
2023-10-05 14:51:25 -0400

    removed it for now

--- kernel/main.c
+++ kernel/main.c
@@ -1,7 +1,6 @@
 void main() {
     printChar('H', 0, 1);
     printChar('I', 1, 1);
-    printStr("Hello world!", 2, 1);
 
     while (1) {
         asm("hlt");
@@ -12,12 +11,4 @@ void printChar(char c, int row, int col) {
     char* vga = (char*)0xb8000;
     vga += 2 * (row * 80 + col);
     *vga = c;
-}
-
-void printStr(char* str, int row, int col) {
-    int i = 0;
-    while (str[i] != '\0') {
-        printChar(str[i], row, col + i);
-        i++;
-    }
 }
