f88b01c241266666965ede998c2cdec9a50f083d
Pigioty <pigiotyreal@gmail.com>
2023-10-05 14:42:42 -0400

    hello

--- kernel/main.c
+++ kernel/main.c
@@ -1,7 +1,16 @@
 void main() {
     char* vga = (char*)0xb8000;
-    
-    *vga = 'X';
+    *vga = 'H';
+    vga += 2;
+    *vga = 'e';
+    vga += 2;
+    *vga = 'l';
+    vga += 2;
+    *vga = 'l';
+    vga += 2;
+    *vga = 'o';
+    vga += 2;
+    *vga = '!';
 
     while (1) {
         asm("hlt");
